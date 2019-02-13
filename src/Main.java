9e1a659baa475a1847a38b2de7436985eaebdb41
ginaioana <gina.crihana@scout.ro>
2019-02-13 20:30:19 +0200

    tema

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello");
-        System.out.println("The addition of 7 an 8 is");
-        System.out.println(7+8);
-        System.out.println(8/2);
-        System.out.println(-5+(8*6));
-        System.out.println((55+9)%9);
-        System.out.println(20+((-3*5)/8));
-        System.out.println(5+((15/3)*2)-8%3);
-       //
-
-        //        int adunare= 7+8;
-      //  System.out.println("Rezultatu adunarii dintre"+ "7"+ "si" +"8" );
-
-      //  Scanner input = new Scanner (System.in);
-      //  System.out.print("8: ");
-       // int a = input.nextInt();
-      //  System.out.print("2: ");
-       // int b = input.nextInt();
-       // int d = (a/b);
-       // System.out.println();
-       // System.out.println("The division of a and b is:" +d);
+//      int res=scadere(  3,  1) ;
+//      System.out.println(res);
+//      System.out.println(scadere(30,10));
 
 
+//    }
+//
+//    public static int scadere( int primulNumar, int alDoileaNumar){
+//    int resultat = primulNumar - alDoileaNumar;
+//    return resultat;
+//    }
 
 
+//
+//        int res=inmultire(  1,  1) ;
+//        System.out.println(res);
+//        System.out.println(inmultire(10,10));
+//
+//
+//    }
+//
+//    public static int inmultire( int primulNumar, int alDoileaNumar){
+//        int resultat = primulNumar * alDoileaNumar;
+//        return resultat;
+//    }
 
-      //  int x = 3;
-      //int y = 4;
-      // String nume= "Gina";
 
-       //double adunare = x + 2.3f;
-      // System.out.println("prima adunare=" + (x + 2.3f));
+        int res = impartire(20, 5);
+        System.out.println(res);
+        System.out.println(impartire(200, 5));
 
 
-     //  int adunare= x + y;
-     // System.out.println("prima adunare=" + adunare);
-     // adunare++;  //  adunare = adunare + 1;
-     //  adunare+=2; // adunare = adunare + 3;
+    }
 
-       // System.out.println("A doua adunare"+adunare);
-
-      //int scadere= x - y;
-     //System.out.println("Rezultatul scaderii dintre "+ x + "si"+ y +"este :"+ scadere);
-
-      // System.out.println(1+2);
-      //System.out.println("Hello World!");
+    public static int impartire(int primulNumar, int alDoileaNumar) {
+        int resultat = primulNumar / alDoileaNumar;
+        return resultat;
     }
 }
