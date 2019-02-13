1bcaee6ff8f0518ac2a47c58d3a01bec02342d3f
ginaioana <gina.crihana@scout.ro>
2019-02-13 17:14:29 +0200

    tema

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,53 @@
 public class Main {
 
     public static void main(String[] args) {
-        int x = 3;
-        int y = 4;
-        String nume= "Gina";
 
-        double adunare = x + 2.3f;
-        System.out.println("prima adunare=" + (x + 2.3f));
+        System.out.println("Hello");
+        System.out.println("The addition of 7 an 8 is");
+        System.out.println(7+8);
+        System.out.println(8/2);
+        System.out.println(-5+(8*6));
+        System.out.println((55+9)%9);
+        System.out.println(20+((-3*5)/8));
+        System.out.println(5+((15/3)*2)-8%3);
+
+       //
+
+        //        int adunare= 7+8;
+      //  System.out.println("Rezultatu adunarii dintre"+ "7"+ "si" +"8" );
+
+      //  Scanner input = new Scanner (System.in);
+      //  System.out.print("8: ");
+       // int a = input.nextInt();
+      //  System.out.print("2: ");
+       // int b = input.nextInt();
+       // int d = (a/b);
+       // System.out.println();
+       // System.out.println("The division of a and b is:" +d);
 
 
-        int adunare= x + y;
-       System.out.println("prima adunare=" + adunare);
-       adunare++;  //  adunare = adunare + 1;
-       adunare+=2; // adunare = adunare + 3;
 
-        System.out.println("A doua adunare"+adunare);
 
-      int scadere= x - y;
-      System.out.println("Rezultatul scaderii dintre "+ x + "si"+ y +"este :"+ scadere);
 
-   //     System.out.println(1+2);
-   //     System.out.println("Hello World!");
+      //  int x = 3;
+      //int y = 4;
+      // String nume= "Gina";
+
+       //double adunare = x + 2.3f;
+      // System.out.println("prima adunare=" + (x + 2.3f));
+
+
+     //  int adunare= x + y;
+     // System.out.println("prima adunare=" + adunare);
+     // adunare++;  //  adunare = adunare + 1;
+     //  adunare+=2; // adunare = adunare + 3;
+
+       // System.out.println("A doua adunare"+adunare);
+
+      //int scadere= x - y;
+     //System.out.println("Rezultatul scaderii dintre "+ x + "si"+ y +"este :"+ scadere);
+
+      // System.out.println(1+2);
+      //System.out.println("Hello World!");
     }
 }
