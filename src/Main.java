4d54e5882352438ee6fa6310fe7419a36df1f274
ginaioana <gina.crihana@scout.ro>
2019-02-13 02:12:18 +0200

    operatiuni

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        int x = 3;
+        int y = 4;
+        String nume= "Gina";
 
-        System.out.println("Hello World!");
+        double adunare = x + 2.3f;
+        System.out.println("prima adunare=" + (x + 2.3f));
+
+
+        int adunare= x + y;
+       System.out.println("prima adunare=" + adunare);
+       adunare++;  //  adunare = adunare + 1;
+       adunare+=2; // adunare = adunare + 3;
+
+        System.out.println("A doua adunare"+adunare);
+
+      int scadere= x - y;
+      System.out.println("Rezultatul scaderii dintre "+ x + "si"+ y +"este :"+ scadere);
+
+   //     System.out.println(1+2);
+   //     System.out.println("Hello World!");
     }
 }
